74a780c0dede58e6cade3b14b73e52e10c557499
ellen-s <ellenschofield5@gmail.com>
2024-09-16 11:56:04 +1000

    Implements objection detection in web viz live feed

--- ui/dashboard/views.py
+++ ui/dashboard/views.py
@@ -6,40 +6,101 @@ from sqlalchemy import inspect, desc, asc
 from datetime import datetime
 # from .forms import TimeRangeForm
 
-# YOLO imports
-# from ultralytics import YOLO
-# import yolov5
-
 # Camera imports
 import cv2
 import depthai as dai
 import time
 
+# TAIP imports
+from pathlib import Path
+import sys
+import numpy as np
+import argparse
+import json
+import blobconverter
+
 bp = Blueprint('main', __name__)
 
 
-# START OF: FOR CAMERA
+# capture_interval = 2  # Set this to your desired interval
+
+# NEW TAIP CONFIG
+# parse config
+configPath = Path('/home/455Team/EGH455-UAV-Project/ui/dashboard/best.json')
+if not configPath.exists():
+    raise ValueError("Path {} does not exist!".format(configPath))
+
+with configPath.open() as f:
+    config = json.load(f)
+nnConfig = config.get("nn_config", {})
+
+# parse input shape
+if "input_size" in nnConfig:
+    W, H = tuple(map(int, nnConfig.get("input_size").split('x')))
+
+# extract metadata
+metadata = nnConfig.get("NN_specific_metadata", {})
+classes = metadata.get("classes", {})
+coordinates = metadata.get("coordinates", {})
+anchors = metadata.get("anchors", {})
+anchorMasks = metadata.get("anchor_masks", {})
+iouThreshold = metadata.get("iou_threshold", {})
+confidenceThreshold = metadata.get("confidence_threshold", {})
+
+print(metadata)
+
+# parse labels
+nnMappings = config.get("mappings", {})
+labels = nnMappings.get("labels", {})
+
+# get model path
+# nnPath = args.model
+nnPath = '/home/455Team/EGH455-UAV-Project/ui/dashboard/best_openvino_2022.1_6shave.blob'
+if not Path(nnPath).exists():
+    print("No blob found at {}. Looking into DepthAI model zoo.".format(nnPath))
+    nnPath = str(blobconverter.from_zoo('best_openvino_2022.1_6shave.blob', shaves = 6, zoo_type = "depthai", use_cache=True))
+# sync outputs
+syncNN = True
+
 # Create pipeline
 pipeline = dai.Pipeline()
 
-# Properties
+# Define sources and outputs
 camRgb = pipeline.create(dai.node.ColorCamera)
-camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A)
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-
-# Define source and output
+detectionNetwork = pipeline.create(dai.node.YoloDetectionNetwork)
 xoutRgb = pipeline.create(dai.node.XLinkOut)
+nnOut = pipeline.create(dai.node.XLinkOut)
+
 xoutRgb.setStreamName("rgb")
-camRgb.video.link(xoutRgb.input)
+nnOut.setStreamName("nn")
 
-# Connect to device
+# Properties
+camRgb.setPreviewSize(W, H)
+
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setInterleaved(False)
+camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+camRgb.setFps(40)
+
+# Network specific settings
+detectionNetwork.setConfidenceThreshold(confidenceThreshold)
+detectionNetwork.setNumClasses(classes)
+detectionNetwork.setCoordinateSize(coordinates)
+detectionNetwork.setAnchors(anchors)
+detectionNetwork.setAnchorMasks(anchorMasks)
+detectionNetwork.setIouThreshold(iouThreshold)
+detectionNetwork.setBlobPath(nnPath)
+detectionNetwork.setNumInferenceThreads(2)
+detectionNetwork.input.setBlocking(False)
+
+# Linking
+camRgb.preview.link(detectionNetwork.input)
+detectionNetwork.passthrough.link(xoutRgb.input)
+detectionNetwork.out.link(nnOut.input)
+
+# *** Connect to device
 device = dai.Device(pipeline)
-capture_interval = 2  # Set this to your desired interval
-# END OF: FOR CAMERA
-
-# START OF: FOR TAIP
-# model = yolov5.load('/home/455Team/EGH455-UAV-Project/ui/dashboard/TAIP_model.pt') 
-# END OF: FOR TAIP
+# END OF: NEW TAIP CONFIG
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -120,38 +181,59 @@ def send_js(path):
 
 
 def get_frame():
-    print('Connected cameras:', device.getConnectedCameraFeatures())
-    print('Usb speed:', device.getUsbSpeed().name)
-    if device.getBootloaderVersion() is not None:
-        print('Bootloader version:', device.getBootloaderVersion())
-    print('Device name:', device.getDeviceName())
+    # # Diagnostic print statements
+    # print('Connected cameras:', device.getConnectedCameraFeatures())
+    # print('Usb speed:', device.getUsbSpeed().name)
+    # if device.getBootloaderVersion() is not None:
+    #     print('Bootloader version:', device.getBootloaderVersion())
+    # print('Device name:', device.getDeviceName())
 
-    # Output queue will be used to get the RGB frames from the output defined above
-    qRgb = device.getOutputQueue(name="rgb", maxSize=30, blocking=False)
-    # qDet = device.getOutputQueue(name="nn", maxSize=4, blocking=False) # DRAFT
+    # Output queues will be used to get the rgb frames and nn data from the outputs defined above
+    qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+    qDet = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
+
+    frame = None
+    detections = []
+    startTime = time.monotonic()
+    counter = 0
+    color2 = (255, 255, 255)
+
+    # nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
+    def frameNorm(frame, bbox):
+        normVals = np.full(len(bbox), frame.shape[0])
+        normVals[::2] = frame.shape[1]
+        return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+
+    def displayFrame(name, frame, detections):
+        color = (255, 0, 0)
+        for detection in detections:
+            bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+            cv2.putText(frame, labels[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 2)
 
     while True:
-        inRgb = qRgb.get()  # blocking call, will wait until new data has arrived
-        # inDet = qDet.get() # DRAFT
+        inRgb = qRgb.get()
+        inDet = qDet.get()
 
-        # with no prediction
-        image = inRgb.getCvFrame()
-        _, jpeg = cv2.imencode('.jpg', image)
-        frame = jpeg.tobytes()
+        if inRgb is not None:
+            frame = inRgb.getCvFrame()
+            cv2.putText(frame, "NN fps: {:.2f}".format(counter / (time.monotonic() - startTime)),
+                        (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color2)
 
-        # with prediction (still working on this)
-        # image = inRgb.getCvFrame()
-        # _, jpeg = cv2.imencode('.jpg', image)
-        # result = model(image)
-        # print(result)
-        # result.save('/results/result.jpg') 
-        # frame = jpeg.tobytes()
+        if inDet is not None:
+            detections = inDet.detections
+            counter += 1
+
+        if frame is not None:
+            displayFrame("rgb", frame, detections)
+            _, jpeg = cv2.imencode('.jpg', frame)
+            frame_with_bbox = jpeg.tobytes()
 
         # time.sleep(capture_interval)
 
         yield (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-            
+            b'Content-Type: image/jpeg\r\n\r\n' + frame_with_bbox + b'\r\n\r\n')
 
 
 @bp.route('/video_feed')
