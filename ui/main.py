ae48497f509a79ac787bcd50297848d626e40adc
Ellen <n9960678@qut.edu.au>
2024-10-08 14:54:30 +1000

    Removes comments

--- ui/main.py
+++ ui/main.py
@@ -13,10 +13,4 @@ app.register_blueprint(bp)
 
 if __name__ == '__main__':
     # app = create_app()
-
-    # # importing views module -> practice to prevent circular references
-    # from dashboard import views
-    # # app.register_blueprint(views.bp)
-    # app.register_blueprint(views.bp, url_prefix='/main')
-
     app.run(debug=False, host='0.0.0.0')
