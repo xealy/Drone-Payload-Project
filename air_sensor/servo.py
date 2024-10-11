90502a291f4c32fe5afc9dde9cd3f855c83fc734
Ellen <n9960678@qut.edu.au>
2024-10-11 11:53:24 +1000

    servo motor testing - doesn't work, unconfirmed gpio pin

---
+++ air_sensor/servo.py
@@ -0,0 +1,27 @@
+from gpiozero import Servo
+from time import sleep
+ 
+myGPIO=4
+ 
+myCorrection=0.5
+maxPW=(2.0+myCorrection)/1000
+minPW=(1.0-myCorrection)/1000
+ 
+servo = Servo(myGPIO,min_pulse_width=minPW,max_pulse_width=maxPW)
+ 
+while True:
+    servo.mid()
+    print("mid")
+    sleep(0.5)
+    servo.min()
+    print("min")
+    sleep(1)
+    servo.mid()
+    print("mid")
+    sleep(0.5)
+    servo.max()
+    print("max")
+    sleep(1)
+    break 
+servo.mid() 
+sleep(0.5)
