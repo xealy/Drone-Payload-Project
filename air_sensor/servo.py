33d0d8ea11863288c213a9308fff62ededb60aff
Ellen <n9960678@qut.edu.au>
2024-10-11 12:03:13 +1000

    Removed "MyGPIO" from servo test

--- air_sensor/servo.py
+++ air_sensor/servo.py
@@ -1,13 +1,7 @@
 from gpiozero import Servo
 from time import sleep
  
-myGPIO=4
- 
-myCorrection=0.5
-maxPW=(2.0+myCorrection)/1000
-minPW=(1.0-myCorrection)/1000
- 
-servo = Servo(myGPIO,min_pulse_width=minPW,max_pulse_width=maxPW)
+servo = Servo(4)
  
 while True:
     servo.mid()
