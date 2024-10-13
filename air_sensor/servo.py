3eef9b9e4805a280eb73245f5e10ef326c81ab72
Ellen <n9960678@qut.edu.au>
2024-10-13 10:06:39 +1000

    Finished servo motor code

--- air_sensor/servo.py
+++ air_sensor/servo.py
@@ -4,13 +4,12 @@ from time import sleep
 
 servo = Servo(13)
 
-for x in range(6):
-    servo.min()
-    sleep(1.0)
+def ServoDrill():
+    servo.max()
+    sleep(15.0)
     servo.mid()
     sleep(2.0)
-    servo.max()
-    sleep(1.0)
-    print("Testing servo loop" + str(x))
-print("Finished testing servo")
+    servo.min()
+    sleep(12.0)
+    
     
